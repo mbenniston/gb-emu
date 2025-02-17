e458c025690958278a032ffcac59f027d7985f1c
mbenniston <matthewpbenniston@gmail.com>
2025-02-17 17:11:38 +0000

    v2-interrupts-panel, Squashed commit of the following:
    commit baa928f6887eaf0bf2e2a47532cda2d84c499ba7
    Author: mbenniston <matthewpbenniston@gmail.com>
    Date:   Mon Feb 17 17:10:06 2025 +0000
    
        feat: added shorthand to IME register
    
    commit 33db0ab279963b6e00b91ca6e654f78db0352443
    Author: mbenniston <matthewpbenniston@gmail.com>
    Date:   Mon Feb 17 07:20:06 2025 +0000
    
        feat: added checkboxes for enable and request interrupt flags

--- src/components/panels/InterruptsPanel.tsx
+++ src/components/panels/InterruptsPanel.tsx
@@ -1,81 +1,63 @@
-import { useState } from "react";
-import {
-  Panel,
-  PanelBody,
-  PanelContent,
-  PanelHeader,
-} from "@/components/Panel.tsx";
-import { useInterrupts } from "@/hooks/useInterrupts.ts";
-import {
-  EditRegisterRow,
-  ViewModeSelectButton,
-} from "@/components/panels/CPURegistersPanel.tsx";
-
-type ViewMode = "decimal" | "hexadecimal";
+import {useId} from "react";
+import {Panel, PanelBody, PanelContent, PanelHeader,} from "@/components/Panel.tsx";
+import {useInterrupts} from "@/hooks/useInterrupts.ts";
 
 export function InterruptsPanel() {
-  const registersQuery = useInterrupts();
-  const [viewMode, setViewMode] = useState<ViewMode>("hexadecimal");
+    const registersQuery = useInterrupts();
 
-  if (!registersQuery.data) return;
+    if (!registersQuery.data) return;
 
-  const registers = registersQuery.data;
+    const registers = registersQuery.data;
 
-  return (
-    <Panel className={"col-span-2 row-span-2"}>
-      <PanelContent>
-        <PanelHeader>Interrupts</PanelHeader>
-        <PanelBody>
-          <ViewModeSelect viewMode={viewMode} onSelectViewMode={setViewMode} />
-          <div className="p-0.5" />
-          <fieldset className="flex flex-col w-full gap-1">
-            <EditRegisterRow
-              register="IME"
-              value={registers.ime}
-              viewMode={viewMode}
-            />
-            <EditRegisterRow
-              register="IE"
-              value={registers.register_ie}
-              viewMode={viewMode}
-            />
-            <EditRegisterRow
-              register="IF"
-              value={registers.register_if}
-              viewMode={viewMode}
-            />
-          </fieldset>
-        </PanelBody>
-      </PanelContent>
-    </Panel>
-  );
+    return (
+        <Panel className={"col-span-2 row-span-2"}>
+            <PanelContent>
+                <PanelHeader>Interrupts</PanelHeader>
+                <PanelBody>
+                    <div className="p-0.5"/>
+                    <fieldset className="">
+                        <div className='grid grid-cols-3 w-full gap-x-1 gap-y-0.5'>
+                            <span className='text-xs'>Name</span>
+                            <span className='text-xs place-self-end'>Enabled</span>
+                            <span className='text-xs place-self-end'>Requested</span>
+                            <InterruptRow shorthand='IME' name='Interrupt master enable' enabled={registers.ime}
+                                          requested={null}/>
+                            <InterruptRow shorthand='Joypad' enabled={registers.joypad_enabled}
+                                          requested={registers.joypad_requested}/>
+                            <InterruptRow shorthand='Serial' enabled={registers.serial_enabled}
+                                          requested={registers.serial_requested}/>
+                            <InterruptRow shorthand='Timer' enabled={registers.timer_enabled}
+                                          requested={registers.timer_requested}/>
+                            <InterruptRow shorthand='LCD' enabled={registers.lcd_enabled}
+                                          requested={registers.lcd_requested}/>
+                            <InterruptRow shorthand='VBlank' enabled={registers.vblank_enabled}
+                                          requested={registers.vblank_requested}/>
+                        </div>
+
+                    </fieldset>
+                </PanelBody>
+            </PanelContent>
+        </Panel>
+    );
 }
 
-function ViewModeSelect({
-  viewMode,
-  onSelectViewMode,
-}: {
-  viewMode: ViewMode;
-  onSelectViewMode: (vm: ViewMode) => void;
+function InterruptRow({name, shorthand, requested, enabled}: {
+    name?: string,
+    shorthand: string,
+    requested: boolean | null,
+    enabled: boolean
 }) {
-  return (
-    <div role="radiogroup" className="text-sm flex gap-1">
-      <ViewModeSelectButton
-        selected={viewMode === "hexadecimal"}
-        onClick={() => {
-          onSelectViewMode("hexadecimal");
-        }}
-      >
-        Hex
-      </ViewModeSelectButton>
-      <ViewModeSelectButton
-        selected={viewMode === "decimal"}
-        onClick={() => {
-          onSelectViewMode("decimal");
-        }}
-      >
-        Dec
-      </ViewModeSelectButton>
-    </div>
-  );
+    const labelId = useId()
+    return <>
+        <label id={labelId} title={name} className='label-text uppercase'>{shorthand}</label>
+        <input aria-labelledby={labelId} type='checkbox' className='checkbox checkbox-xs place-self-end' readOnly
+               checked={enabled}/>
+        {requested !== null &&
+            <input aria-labelledby={labelId} type='checkbox' className='checkbox checkbox-xs place-self-end' readOnly
+                   checked={requested}/>}
+        {requested === null &&
+            <span/>}
+
+    </>
 }
+
